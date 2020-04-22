eafd7e33a5a30195189d2dbcb1a6a2bc73c7b8a1
sweta222 <swetastuti2@gmail.com>
2020-04-22 13:51:18 +0530

    added all data fetching and rendering stuffs to show  user details(github info) as requested by a user

--- my-todo/src/App.js
+++ my-todo/src/App.js
@@ -1,126 +1,179 @@
 // import React from 'react';
-// import logo from './logo.svg';
+// import ReactDOM from 'react-dom';
 // import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { userID: '' };
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handlesubmit = this.handlesubmit.bind(this);
+//   }
+//   handleChange(event) {
+//     this.setState({ userID: event.target.value });
+//   }
+//   handlesubmit(event) {
+//     event.preventDefault();
+//     alert('submitted userID is' + this.state.userID);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1 className="heading">Github User Details</h1>
+//         <form onSubmit={this.handlesubmit}>
+//           <input
+//             type="text"
+//             className="userid"
+//             placeholder="userID"
+//             onChange={this.handleChange}
+//           />
+//           <br />
+//           <br />
+//           <input type="submit" className="fetch" value="fetch details" />
+//         </form>
+//       </div>
+//     );
+//   }
 // }
-
 // export default App;
-import React from 'react';
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// import React from 'react';
+// import {useState} from 'react';
+// import './App.css';
+// const App = () => {
+//   const [userID,setuserID] = useState('');
+
+//   const handlesubmit = (event) => {
+//     event.preventDefault();
+//     alert('submitted userID is' + userID);
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <h1 className="heading">Github User Details</h1>
+//         <form onSubmit={handlesubmit}>
+//           <input
+//             type="text"
+//             className="userid"
+//             placeholder="userID"
+//             onChange={(event) => setuserID(event.target.value)}
+//           />
+//           <br />
+//           <br />
+//           <input type="submit" className="fetch" value="fetch details" />
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+// export default App;
+
+import React, { useState, useEffect } from 'react';
+import { Form, Card, Image, Icon } from 'semantic-ui-react';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      newItem: '',
-      list: [],
-    };
-  }
+const App = () => {
+  const [name, setName] = useState('');
+  const [userName, setuserName] = useState('');
+  const [followers, setFollowers] = useState('');
+  const [following, setFollowing] = useState('');
+  const [repos, setRepos] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [userInput, setUserInput] = useState('');
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState(null);
 
-  addItem(todoValue) {
-    if (todoValue !== '') {
-      const newItem = {
-        id: Date.now(),
-        value: todoValue,
-        isDone: false,
-      };
-      const list = [...this.state.list];
-      list.push(newItem);
-      this.setState({
-        list,
-        newItem: '',
+  useEffect(() => {
+    fetch('https://api.github.com/users/example')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
       });
-    }
-  }
+  }, []);
+  const setData = ({
+    name,
+    login,
+    followers,
+    following,
+    public_repos,
+    avatar_url,
+    email,
+  }) => {
+    setName(name);
+    setuserName(login);
+    setFollowers(followers);
+    setFollowing(following);
+    setRepos(public_repos);
+    setAvatar(avatar_url);
+    setEmail(email);
+  };
 
-  deleteItem(id) {
-    const list = [...this.state.list];
-    const updatedList = list.filter((item) => item.id !== id);
-    this.setState({ list: updatedList });
-  }
+  // const handleSearch = (e) => {
+  //   setUserInput(e.target.value);
+  // };
 
-  updateInput(input) {
-    this.setState({ newItem: input });
-  }
+  const handlesubmit = () => {
+    fetch(`https://api.github.com/users/${userInput}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message) {
+          setError(data.message);
+        } else {
+          setData(data);
+        }
+      });
+  };
 
-  // updateCheck(isDone) {
-  //   const list = [...this.state.list];
-  //   if (!isDone) {
-  //     this.setState({ list });
-  //   }
-  // }
-  render() {
-    return (
-      <div>
-        <h1 className="header">Todo App</h1>
-        <div className="container">
-          Add an item
-          <br />
-          <input
-            type="text"
-            className="input-text"
-            placeholder="write a todo"
-            required
-            value={this.state.newItem}
-            onChange={(e) => this.updateInput(e.target.value)}
-          />
-          <button
-            className="add-btn"
-            onClick={() => this.addItem(this.state.newItem)}
-            disabled={!this.state.newItem.length}
-          >
-            Add todo
-          </button>
-          <div className="list">
-            <ul>
-              {this.state.list.map((item) => {
-                return (
-                  <li key={item.id}>
-                    <input
-                      type="checkbox"
-                      name="idDone"
-                      checked={item.isDone}
-                      onChange={() => {}}
-                    />
-                    {item.value}
-                    <button
-                      className="btn"
-                      onClick={() => this.deleteItem(item.id)}
-                    >
-                      Delete
-                    </button>
-                  </li>
-                );
-              })}
-              {/* <li>
-                <input type="checkbox" name="" id="" />
-                todo's list
-                <button className="btn">Delete</button>
-              </li> */}
-            </ul>
-          </div>
-        </div>
+  return (
+    <div>
+      <div className="heading">Github User Details</div>
+      <div className="search">
+        <Form onSubmit={handlesubmit}>
+          <Form.Group>
+            <Form.Input
+              placeholder="github username"
+              name="name"
+              onChange={(event) => setUserInput(event.target.value)}
+            />
+            <Form.Button content="search" />
+          </Form.Group>
+        </Form>
       </div>
-    );
-  }
-}
+      {error ? (
+        <h1>{error}</h1>
+      ) : (
+        <div className="card">
+          <Card>
+            <Image src={avatar} wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>{name}</Card.Header>
+              <Card.Header>{userName}</Card.Header>
+              <Card.Header>{email}</Card.Header>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name="user" />
+                {followers} Followers
+              </a>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name="user" />
+                {repos} Repos
+              </a>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name="user" />
+                {following} Following
+              </a>
+            </Card.Content>
+          </Card>
+        </div>
+      )}
+    </div>
+  );
+};
 export default App;
