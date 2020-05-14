02fa5ab64397ec15f79d49fb18e94ae734d2ec27
sweta222 <swetastuti2@gmail.com>
2020-05-14 13:27:47 +0530

    added type definition for reducer function's parametres(arguements)

---
+++ src/store/reducer.ts
@@ -0,0 +1,39 @@
+import { combinedAction, initialState, CHANGE, SUBMIT } from '../interfaces';
+const initState: initialState = {
+  name: '',
+  username: '',
+  email: '',
+  repos: 0,
+  following: 0,
+  followers: 0,
+  error: null,
+  avatar: '',
+  grabData: false,
+};
+const reducer = (state = initState, action: combinedAction): initialState => {
+  if (action.type === CHANGE) {
+    return {
+      ...state,
+      username: (action.e.target as HTMLInputElement).value,
+    };
+  } else if (action.type === SUBMIT) {
+    console.log(action.data);
+    if (action.data.public_repos) {
+      return {
+        ...state,
+        name: action.data.name,
+        email: action.data.email,
+        repos: action.data.public_repos,
+        following: action.data.following,
+        followers: action.data.followers,
+        error: action.data.message,
+        avatar: action.data.avatar_url,
+        grabData: true,
+      };
+    } else {
+      return { ...state, error: 'User Not Found' };
+    }
+  }
+  return state;
+};
+export default reducer;
