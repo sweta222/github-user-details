df697b685693be0cf4d73b9236aa8b9d18ef331d
sweta222 <swetastuti2@gmail.com>
2020-05-19 14:01:54 +0530

    replaced switch case in place of if else for reducer function for convinient display of data(incase of errors)

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -12,14 +12,13 @@ const initState: initialState = {
   grabData: false,
 };
 const reducer = (state = initState, action: combinedAction): initialState => {
-  if (action.type === CHANGE) {
-    return {
-      ...state,
-      username: (action.e.target as HTMLInputElement).value,
-    };
-  } else if (action.type === SUBMIT) {
-    console.log(action.data);
-    if (action.data.public_repos) {
+  switch (action.type) {
+    case CHANGE:
+      return {
+        ...state,
+        username: (action.e.target as HTMLInputElement).value,
+      };
+    case SUBMIT:
       return {
         ...state,
         loginId: action.data.login,
@@ -32,10 +31,37 @@ const reducer = (state = initState, action: combinedAction): initialState => {
         avatar: action.data.avatar_url,
         grabData: true,
       };
-    } else {
-      return { ...state, error: 'User Not Found' };
-    }
+    default:
+      return state;
   }
-  return state;
 };
 export default reducer;
+
+// const reducer = (state = initState, action: combinedAction): initialState => {
+//   if (action.type === CHANGE) {
+//     return {
+//       ...state,
+//       username: (action.e.target as HTMLInputElement).value,
+//     };
+//   } else if (action.type === SUBMIT) {
+//     //console.log(action.data);
+//     if (action.data.public_repos) {
+//       return {
+//         ...state,
+//         loginId: action.data.login,
+//         name: action.data.name,
+//         email: action.data.email,
+//         repos: action.data.public_repos,
+//         following: action.data.following,
+//         followers: action.data.followers,
+//         error: action.data.message,
+//         avatar: action.data.avatar_url,
+//         grabData: true,
+//       };
+//     } else {
+//       return { ...state, error: 'User Not Found' };
+//     }
+//   }
+//   return state;
+// };
+// export default reducer;
