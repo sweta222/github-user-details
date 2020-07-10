1b8fbc0c07f2d7162944e28a86fc4f8188035dcd
sweta222 <swetastuti2@gmail.com>
2020-07-10 17:18:31 +0530

    added repos,contributions,yearlycontributions,blog,company in state and reducer function(so that it'll return updated state according to the logic provided in switch case expressions

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,15 +1,33 @@
-import { combinedAction, initialState, CHANGE, SUBMIT } from '../interfaces';
+import {
+  combinedAction,
+  initialState,
+  CHANGE,
+  SUBMIT,
+  ADD_REPOS,
+  //ADD_COMMITS,
+  ADD_CONTRIBUTIONS,
+  ADD_YEARLY_CONTRIBUTIONS,
+} from '../interfaces';
 const initState: initialState = {
   loginId: '',
   name: '',
   username: '',
   email: '',
-  repos: 0,
+  repositories: 0,
   following: 0,
   followers: 0,
-  error: null,
+  error: '',
   avatar: '',
   grabData: false,
+  profile_url: '',
+  location: '',
+  blog: '',
+  repos: [],
+  //commits: [],
+  contributions: [],
+  yearlyContributionsData: [],
+  bio: '',
+  company: '',
 };
 const reducer = (state = initState, action: combinedAction): initialState => {
   switch (action.type) {
@@ -24,44 +42,37 @@ const reducer = (state = initState, action: combinedAction): initialState => {
         loginId: action.data.login,
         name: action.data.name,
         email: action.data.email,
-        repos: action.data.public_repos,
+        repositories: action.data.public_repos,
         following: action.data.following,
         followers: action.data.followers,
         error: action.data.message,
         avatar: action.data.avatar_url,
+        profile_url: action.data.html_url,
         grabData: true,
+        location: action.data.location,
+        blog: action.data.blog,
+        bio: action.data.bio,
+        company: action.data.company,
+      };
+    case ADD_REPOS:
+      return {
+        ...state,
+        repos: action.repos,
+      };
+    // case ADD_COMMITS:
+    //   return {
+    //     ...state,
+    //     commits: action.commits,
+    //   };
+    case ADD_CONTRIBUTIONS:
+      return { ...state, contributions: action.contributions };
+    case ADD_YEARLY_CONTRIBUTIONS:
+      return {
+        ...state,
+        yearlyContributionsData: action.yearlyContributionsData,
       };
     default:
       return state;
   }
 };
 export default reducer;
-
-// const reducer = (state = initState, action: combinedAction): initialState => {
-//   if (action.type === CHANGE) {
-//     return {
-//       ...state,
-//       username: (action.e.target as HTMLInputElement).value,
-//     };
-//   } else if (action.type === SUBMIT) {
-//     //console.log(action.data);
-//     if (action.data.public_repos) {
-//       return {
-//         ...state,
-//         loginId: action.data.login,
-//         name: action.data.name,
-//         email: action.data.email,
-//         repos: action.data.public_repos,
-//         following: action.data.following,
-//         followers: action.data.followers,
-//         error: action.data.message,
-//         avatar: action.data.avatar_url,
-//         grabData: true,
-//       };
-//     } else {
-//       return { ...state, error: 'User Not Found' };
-//     }
-//   }
-//   return state;
-// };
-// export default reducer;
